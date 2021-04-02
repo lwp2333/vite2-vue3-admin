f04c8edcadf02c325d31c7d1add4c99c7b00283e
liwp <liwp@hsmap.com>
2021-04-02 16:11:38 +0800

    feat: import.meta.globEager 使用

--- src/router/asyncRoutes.js
+++ src/router/asyncRoutes.js
@@ -1,10 +1,8 @@
 // 导入模块
-import dashBoard from './modules/dashBoard'
-import form from './modules/form'
-import table from './modules/table'
-import setting from './modules/setting'
-import multiLevelMenu from './modules/multiLevelMenu'
-import exampleCenter from './modules/exampleCenter'
-const asyncRoutes = [dashBoard, form, table, setting, exampleCenter, multiLevelMenu]
-console.log(asyncRoutes)
+const modules = import.meta.globEager('./modules/*.js')
+const asyncRoutes = []
+Object.keys(modules).forEach(file => {
+  asyncRoutes.push(modules[file].default)
+})
+
 export default asyncRoutes
